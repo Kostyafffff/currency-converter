397be37854130b3752dc83d9100853f025cb1186
Kostya <konstantin.furmanov@gmail.com>
2021-01-18 23:28:45 +0200

    Add select with list of currency

--- src/components/latest/base-currency/BaseCurrencyComponent.tsx
+++ src/components/latest/base-currency/BaseCurrencyComponent.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { IPropsBaseCurrency } from './types';
 import Select from '@material-ui/core/Select';
+import {MenuItem} from "@material-ui/core";
 
 export const BaseCurrencyComponent: React.FC<IPropsBaseCurrency> = ({currencies, baseCurrency}) => {
+    const [currency, setCurrency] = useState(currencies[0]);
 
+    // const updateValue = (ev: any): void => {
+    //     setCurrency(ev.currentTarget.value)
+    // }
     return (
         <div>
             <Select
-                label={baseCurrency}
-                native={true}
-                defaultValue={baseCurrency}
-            />
-
+                style={{width: 120}}>
+                {currencies.map(it => (<MenuItem>{it}</MenuItem>))}
+            </Select>
         </div>
     );
 }
---
+++ src/components/latest/base-currency/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+
+export const SelectWrapper = styled.div`
+  .MuiInput-root {
+    width: 20px;
+  }
+`;
