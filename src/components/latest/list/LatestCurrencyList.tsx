7d7e9c624c3b2d839b633f3fca396db0dd95d02f
Kostya <konstantin.furmanov@gmail.com>
2021-01-24 23:51:46 +0200

    add columns

--- src/components/latest/list/LatestCurrencyList.tsx
+++ src/components/latest/list/LatestCurrencyList.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import {IPropsLatestCurrencyList} from "./types";
-import { Table } from '@material-ui/core';
+import {Table, TableBody, TableCell, TableHead, TableRow} from '@material-ui/core';
 
 
 export const LatestCurrencyList: React.FC<IPropsLatestCurrencyList> = ({ rates }) => {
 
     return(
-        <Table />
+        <Table>
+            <TableHead>
+                <TableCell>
+                    Currency
+                </TableCell>
+                <TableCell align={"left"}>
+                    Rate
+                </TableCell>
+            </TableHead>
+            <TableBody>
+                {
+                    <TableRow>
+                    </TableRow>
+                }
+            </TableBody>
+        </Table>
     )
 }
