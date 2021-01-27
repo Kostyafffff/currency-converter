ca207f459f3f70d69a0eae621aefb10b0463b422
Kostya <konstantin.furmanov@gmail.com>
2021-01-27 23:16:31 +0200

    Add some styles

--- src/components/latest/container/styles.ts
+++ src/components/latest/container/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const LatestViewContainerWrapper = styled.div`
 width: 900px;
-height: 700px;
+height: auto;
 border: 1px solid black;
 position: absolute;
 top: 10%;
