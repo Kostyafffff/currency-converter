213ed095c7ad7f5954d21e3d25afb50a79e6ec05
Kostya <konstantin.furmanov@gmail.com>
2021-02-01 21:06:03 +0200

    add handle change tabs

--- src/components/tabs/TabComponent.tsx
+++ src/components/tabs/TabComponent.tsx
@@ -1,16 +1,21 @@
 import React, {useCallback, useState} from "react";
 import { TAB_VALUE } from "./types";
+import { Tab } from "@material-ui/core";
+import {TabsStyled} from "./styles";
 
 export const TabComponent: React.FC = () => {
 
-    const [tab, setTab] = useState(TAB_VALUE.LATEST);
+    const [tab, setTab] = useState(0);
 
-    const onTabChange = useCallback((): void => {
-        setTab(tab)
-    }, [setTab])
+    const onTabChange = useCallback((event: React.ChangeEvent<{}>, newValue: number): void => {
+        setTab(newValue);
+    }, [setTab]);
 
     return (
-        <div>
-
-        </div>)
+        <TabsStyled value={tab} onChange={onTabChange}>
+             <Tab label={TAB_VALUE.LATEST} />
+             <Tab label={TAB_VALUE.CONVERTER}  />
+             <Tab label={TAB_VALUE.HISTORY} />
+        </TabsStyled>
+    )
 }
---
+++ src/components/tabs/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import {Tabs} from "@material-ui/core";
+
+export const TabsStyled = styled(Tabs)`
+  border: 1px solid black;
+  width: 900px;
+  
+  .MuiTabs-flexContainer {
+    justify-content: space-between;
+  }
+`;
