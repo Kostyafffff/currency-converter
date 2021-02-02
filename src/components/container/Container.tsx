fa3eb8ceaa72a853d95acdccd3a53435f410ba51
Kostya <konstantin.furmanov@gmail.com>
2021-02-02 22:42:09 +0200

    fix styles

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {LatestViewContainer} from "../latest/container/LatestViewContainer";
 import {TabComponent} from "../tabs/TabComponent";
+import {ContainerStyled} from "./styles";
 
 export const Container: React.FC = () => {
 
   return (
-    <div className="App">
+      <ContainerStyled>
         <TabComponent />
         <LatestViewContainer />
-    </div>
+      </ContainerStyled>
   );
 }
--- src/components/latest/container/styles.ts
+++ src/components/latest/container/styles.ts
@@ -4,7 +4,6 @@ export const LatestViewContainerWrapper = styled.div`
 width: 900px;
 height: auto;
 border: 1px solid black;
-position: absolute;
 top: 10%;
 left: 25%
 `;
