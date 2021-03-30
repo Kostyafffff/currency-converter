32924517fdcd8a6ab4ef76c1fd04208ec3f9d8b7
Kostya <konstantin.furmanov@gmail.com>
2021-03-30 23:22:21 +0300

    add update use effect

--- src/components/history/container/HistoryView.tsx
+++ src/components/history/container/HistoryView.tsx
@@ -8,24 +8,24 @@ import { BaseCurrencyComponent } from "../../latest/base-currency/BaseCurrencyCo
 export const HistoryView: React.FC = () => {
     const [currencyRates, setCurrencyRates] = useState<ICurrencyRate>();
     const [startDate, setStartDate] = useState('2017-05-24');
-    const [currency, setCurrency] = useState<string>('USD');
+    const [currency, setCurrency] = useState<string>('');
 
     let rates: string[] = [];
     if(currencyRates !== undefined) {
         rates = Object.keys(currencyRates.rates);
     }
 
-
-
-
+    // const setCurrencyRate = useCallback(() => {
+    //     setCurrency(currency);
+    // }, [currency, setCurrency]);
 
     const loadCurrencyRates = useCallback((): void => {
-        const url = `https://api.exchangeratesapi.io/${startDate}`;
+        const url = `https://api.exchangeratesapi.io/${startDate}?base=${currency}`;
 
         fetch(url)
             .then(response => response.json())
             .then(data => setCurrencyRates(data));
-    }, [startDate, setCurrencyRates]);
+    }, [startDate, setCurrencyRates, currency]);
 
 
     useEffect(() => {
@@ -56,7 +56,7 @@ export const HistoryView: React.FC = () => {
             <LatestCurrencyList rates={currencyRates.rates} />
             <BaseCurrencyComponent
                 baseCurrency={currency}
-                onChangeBaseCurrency={() => {}}
+                onChangeBaseCurrency={loadCurrencyRates}
                 currencies={rates}
             />
         </HistoryViewContainer>
